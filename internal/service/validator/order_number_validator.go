7a9db215f4bad4de54ee0927102f67b71f030fe1
i.konstantionov <i.konstantionov@bi.zone>
2026-05-12 01:31:32 +0300

    ref

--- internal/service/validator/order_number_validator.go
+++ internal/service/validator/order_number_validator.go
@@ -6,30 +6,23 @@ import (
 )
 
 func ValidateLuhn(number string) bool {
-	// 1. Удаляем пробелы, если они есть
 	number = strings.ReplaceAll(number, " ", "")
 
 	var sum int
 	var alternate bool
-
-	// 2. Идем с конца строки
 	for i := len(number) - 1; i >= 0; i-- {
 		n, err := strconv.Atoi(string(number[i]))
 		if err != nil {
 			return false // Нецифровой символ
 		}
-
 		if alternate {
 			n *= 2
 			if n > 9 {
 				n -= 9
 			}
 		}
-
 		sum += n
 		alternate = !alternate
 	}
-
-	// 3. Сумма должна делиться на 10
 	return sum%10 == 0
 }
